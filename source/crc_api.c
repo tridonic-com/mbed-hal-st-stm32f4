c2563cadde5edc70510ea22d4a1b6d7f4866c6f3
Daniel Cesarini <daniel.cesarini@tridonic.com>
2016-07-05 17:10:23 +0200

    Working version of CRC calculation of F4 devices
    - Implemented the CalcSingle and the CalcAccumulate functions. Only the CalcAccumulate has been tested until now.
    - Added header comment of file

--- source/crc_api.c
+++ source/crc_api.c
@@ -1,9 +1,21 @@
-/*
- * crc_api.h
+/*************************************************************************//**
  *
- *  Created on: 11. Apr. 2016
- *      Author: martin.tomasini2
- */
+ * \file    crc_api.c
+ * \brief   ...
+ *
+ * \note    None
+ *
+ * \author  Daniel Cesarini <daniel.cesarini@tridonic.com>
+ * \author  Martin Tomasini <martin.tomasini2@tridonic.com>
+ * \version 0.1
+ * \date    4 July 2016
+ * \bug     None
+ *
+ * \copyright 2016, Tridonic GmbH & Co KG
+ *                  Färbergasse 15 - 6851 Dornbirn, Austria
+ *
+ *****************************************************************************/
+
 #include "crc_api.h"
 #include "cmsis.h"
 
@@ -21,18 +33,78 @@ int Init()
 
 int Reset()
 {
+    /* Reset CRC Calculation Unit */
+    __HAL_CRC_DR_RESET(&CRC_HandleStruct);
 
+    /*HAL_CRC_DeInit(&CRC_HandleStruct);
+    HAL_CRC_Init(&CRC_HandleStruct);*/
 }
 
-int CalcSingle(uint8_t* ui32StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC)
+int CalcSingle(uint8_t* pui8StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC)
 {
-   *pui32CRC = 2;
-   return 0;
+    uint32_t *pui32StartAddress = (uint32_t*)pui8StartAddress;
+    uint32_t ui32DataSize32Bits = ui32DataSize / 4;
+
+    for (uint32_t i = 0; i < ui32DataSize32Bits; i++)
+    {
+        pui32StartAddress[i] = __RBIT(pui32StartAddress[i]);
+        pui32StartAddress[i] = __REV(pui32StartAddress[i]);
+    }
+
+    *pui32CRC = HAL_CRC_Calculate(&CRC_HandleStruct, (uint32_t*)pui8StartAddress, ui32DataSize32Bits);
+
+    // Reverse back the data (otherwise it will leave the data changed)
+    for (uint32_t i = 0; i < ui32DataSize32Bits; i++)
+    {
+        pui32StartAddress[i] = __REV(pui32StartAddress[i]);
+        pui32StartAddress[i] = __RBIT(pui32StartAddress[i]);
+    }
+
+    // reverse CRC result before final XOR
+    *pui32CRC = __RBIT(*pui32CRC);
+    *pui32CRC ^= 0xffffffff;
+
+    return 0;
 }
 
-int CalcAccumulate(uint8_t* ui32StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC)
+int CalcAccumulate(uint8_t* pui8StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC)
 {
-    *pui32CRC = 3;
+    uint32_t *pui32StartAddress = (uint32_t*)pui8StartAddress;
+    uint32_t ui32DataSize32Bits = ui32DataSize / 4;
+    uint32_t ui32TemporaryDataStorage;
+
+    // Reverse input bits
+    for (uint32_t i = 0; i < ui32DataSize32Bits; i++)
+    {
+        ui32TemporaryDataStorage = pui32StartAddress[i];
+        ui32TemporaryDataStorage = __RBIT(ui32TemporaryDataStorage);
+        //pui32StartAddress[i] = __REV(pui32StartAddress[i]);   // DECOMMENTED BECAUSE IT IS NOT NEEDED. DISCUSS WITHN mab TODO!!!
+        *pui32CRC = HAL_CRC_Accumulate(&CRC_HandleStruct, &ui32TemporaryDataStorage, 1);
+    }
+
+   /*
+
+    // Reverse input bits
+    for (uint32_t i = 0; i < ui32DataSize32Bits; i++)
+    {
+        pui32StartAddress[i] = __RBIT(pui32StartAddress[i]);
+        pui32StartAddress[i] = __REV(pui32StartAddress[i]);
+    }
+
+    *pui32CRC = HAL_CRC_Accumulate(&CRC_HandleStruct, (uint32_t*)pui8StartAddress, ui32DataSize32Bits);
+
+    // Reverse back the data (otherwise it will leave the data changed)
+    for (uint32_t i = 0; i < ui32DataSize32Bits; i++)
+    {
+        pui32StartAddress[i] = __REV(pui32StartAddress[i]);
+        pui32StartAddress[i] = __RBIT(pui32StartAddress[i]);
+    }
+    */
+
+    // reverse CRC result before final XOR
+    *pui32CRC = __RBIT(*pui32CRC);
+    *pui32CRC ^= 0xffffffff;
+
     return 0;
 }
 
